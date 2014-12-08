2f936599639cc43509068fc1cb96b722021153e0
Utkarsh Sharma <sharma.utkarsh.iitd@gmail.com>
2014-12-08 20:23:23 +0900

    SPOJ problem id: TEST

---
+++ SPOJ-test.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+while (true)
+{
+    int in;
+    cin >> in;
+    if (in != 42)
+        cout << in << endl;
+    else
+        return 0;
+}
+}
