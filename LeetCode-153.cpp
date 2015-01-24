3076b282ec99dde174774fdf67f99da4c84e4c89
Utkarsh Sharma <sharma.utkarsh.iitd@gmail.com>
2015-01-24 16:40:44 +0530

    LeetCodeOJ. Find Minimum in Rotated Sorted Array.

---
+++ LeetCode-153.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <bitset>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int> &prices)
+    {
+        int low = 0;
+        int high = prices.size() - 1;
+        if (prices[high] > prices[low])
+            return prices[low];
+        while (high - low > 1)
+        {
+            int mid = (low+high)/2;
+//            cout << "low is " << low << " mid is " << mid << " and high is " << high << endl;
+//            cout << "plow is " << prices[low] << " pmid is " << prices[mid] << " and phigh is " << prices[high] << endl << endl;
+            if (prices[mid] > prices[low])
+            {
+                low = mid;
+            }
+            else if (prices[mid] < prices[low])
+            {
+                high = mid;
+            }
+        }
+//        cout << "final low and high are  " << low << " " << high << endl;
+        return min(prices[low], prices[high]);
+    }
+};
+
+int main()
+{
+vector<int> test;
+test.push_back(4);
+test.push_back(5);
+test.push_back(6);
+test.push_back(1);
+test.push_back(2);
+test.push_back(3);
+Solution obj;
+int ans = obj.findMin(test);
+cout << "ans is " << ans << endl;
+return 0;
+}
