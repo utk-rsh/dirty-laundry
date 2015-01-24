0d6b2c63c5d9e1fcd1ee360b0be9926bff02f511
Utkarsh Sharma <sharma.utkarsh.iitd@gmail.com>
2015-01-24 16:01:09 +0530

    Corrected error. Had overwritten this file and committed.

--- LeetCode-59.cpp
+++ LeetCode-59.cpp
@@ -11,67 +11,61 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>> &matrix)
+    vector<vector<int> > generateMatrix(int n)
     {
-        int m = matrix.size();
-        vector<int> ans;
-        if (m == 0)
-            return ans;
-        int n = matrix[0].size();
+        vector<vector<int>> ans;
+        vector<int> dummy (n);
+        for (int i = 0; i < n; i++)
+            dummy[i] = 0;
+        for (int i = 0; i < n; i++)
+            ans.push_back(dummy);
         int top = 1;
         int left = 0;
         int right = n-1;
-        int bottom = m-1;
+        int bottom = n-1;
         int counter = 1;
         int x = 0;
         int y = 0;
-        cout << m*n << " ... " << endl;
-        while (counter <= m*n)
+        while (counter <= n*n)
         {
      //       cout << "Right is " << right << endl;
             while(y <= right)
             {
      //           cout << "x is " << x << " y is " << y << " and counter is " << counter << endl;
-                ans.push_back(matrix[x][y]);
+                ans[x][y] = counter;
                 counter++;
                 y++;
             }
-            if (counter > m*n)
-                break;
             x++;
             y--;
             right--;
-       //     cout << "Bottom is " << bottom << endl;
+     //       cout << "Bottom is " << bottom << endl;
             while (x <= bottom)
             {
-           //     cout << "x is " << x << " y is " << y << " and counter is " << counter << endl;
-                ans.push_back(matrix[x][y]);
+     //           cout << "x is " << x << " y is " << y << " and counter is " << counter << endl;
+                ans[x][y] = counter;
                 counter++;
                 x++;
             }
-            if (counter > m*n)
-                break;
             x--;
             y--;
             bottom--;
-         //   cout << "Left is " << left << endl;
+     //       cout << "Left is " << left << endl;
             while (y >= left)
             {
-       //         cout << "x is " << x << " y is " << y << " and counter is " << counter << endl;
-                ans.push_back(matrix[x][y]);
+     //           cout << "x is " << x << " y is " << y << " and counter is " << counter << endl;
+                ans[x][y] = counter;
                 counter++;
                 y--;
             }
-            if (counter > m*n)
-                break;
             y++;
             x--;
             left++;
-     //       cout << "Top is " << top << endl;
+    //        cout << "Top is " << top << endl;
             while (x >= top)
             {
     //            cout << "x is " << x << " y is " << y << " and counter is " << counter << endl;
-                ans.push_back(matrix[x][y]);
+                ans[x][y] = counter;
                 counter++;
                 x--;
             }
@@ -82,5 +76,3 @@ public:
         return ans;
     }
 };
-
-\
