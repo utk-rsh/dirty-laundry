56b83c7ce4bbaf39683745b3351c280746ec4413
Utkarsh Sharma <sharma.utkarsh.iitd@gmail.com>
2015-12-04 13:59:07 +0530

    LeetCodeOJ. First Bad Version

---
+++ LeetCode-278.cc
@@ -0,0 +1,22 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+  int firstBadVersion(int n) {
+    int low = 1;
+    int high = n;
+    while (low != high)
+    {
+      int mid = low + (high - low)/2;
+      if (isBadVersion(mid))
+        high = mid;
+      else
+        low = mid + 1;        
+    }
+    if (isBadVersion(low))
+      return low;
+    else
+      return low+1;
+  }
+};
